c258bff183d5952b9204987c6143cc25841b6472
KocAlex <alex.koca@alessandrinimainardi.edu.it>
2023-10-05 13:38:47 +0200

    Added variabili.html and index.html

--- prima_app/urls.py
+++ prima_app/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from prima_app.views import homepage, welcome, lista, chi_siamo
+from prima_app.views import homepage, welcome, lista, chi_siamo, variabili, index
 app_name = "prima_app"
 urlpatterns = [
-            path("", homepage, name = "homepage"),
+            path("", index, name = "index"),
+            path("homepage", homepage, name = "homepage"),
             path("welcome", welcome, name = "welcome"),
             path("lista", lista, name = "lista"),
             path("chi_siamo", chi_siamo, name = "chi_siamo"),
+            path("variabili", variabili, name = "variabili"),
             ]
--- prima_app/views.py
+++ prima_app/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, "index.html")
+
 def homepage(request):
     return render(request, "homepage.html")
 
@@ -12,3 +15,12 @@ def lista(request):
 
 def chi_siamo(request):
     return render(request, "chi_siamo.html")
+
+def variabili(request):
+    context = {"var1" : "Prima variabile",
+               "var2" : "Seconda variabile",
+               "var3" : "Terza variabile"
+               }
+    return render(request, "variabili.html", context)
+
+
