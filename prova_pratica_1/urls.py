7ba36812f96daa6a3fef66b9df195dee87946af3
KocAlex <alex.koca@alessandrinimainardi.edu.it>
2023-11-03 10:17:46 +0100

    Cambiamento da [NOMEPAGINA] a 'view_a', 'view_b', 'view_c'

--- prova_pratica_1/urls.py
+++ prova_pratica_1/urls.py
@@ -3,7 +3,7 @@ from prova_pratica_1.views import index, somma, media, voti
 app_name = "prova_pratica_1"
 urlpatterns = [
             path("", index, name = "index"),
-            path("somma", somma, name = "somma"),
-            path("media", media, name = "media"),
-            path("voti", voti, name = "voti"),
+            path("view_a", somma, name = "somma"),
+            path("view_b", media, name = "media"),
+            path("view_c", voti, name = "voti"),
             ]
