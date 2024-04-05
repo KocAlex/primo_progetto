bdfd1a67df3c342be4a59dda9759c2b0030570b6
KocAlex <alex.koca@alessandrinimainardi.edu.it>
2024-04-05 09:02:46 +0200

    Update urls.py

--- primo_progetto/urls.py
+++ primo_progetto/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     path('prima_app/', include("prima_app.urls", namespace = "prima_app")),
     path('seconda_app/', include("seconda_app.urls", namespace = "seconda_app")),
     path('news/', include("news.urls", namespace = "news")),
-    path('store/', include("products.urls", namespace = "store")), #??
+    path('store/', include("products.urls", namespace = "store")), 
     path('prova_pratica_1/', include("prova_pratica_1.urls", namespace = "prova_pratica_1")),
     path('', index, name = 'index'),
 ]
